4f0415e60757215505d1e3a8f07efd292aff8ee3
dani morillo <danifromecuador@gmail.com>
2023-04-20 13:41:19 -0500

    Create likes counter with involvment API

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
-import './styles.css';
-import { apiGet } from './modules/api.js';
+// import './styles.css';
+import { apiGet, sendLike, getLikes } from './modules/api.js';
 
 const mainBody = document.getElementById('main');
 const popup = document.getElementById('popup-window');
 
-function generateMovies(episode) {
-  return `
-    <div id = "episode-card-${episode.number}" class = "episode-card">
-      <div class = "episode-img" id = "episode-img-${episode.number}">
-        <img src = "${episode.image.medium}">
+window.addEventListener('load', async () => {
+  const numberOfLikes = await getLikes();
+  const epiArray = await apiGet();
+  for (let i = 0; i < epiArray.length; i++) {
+    if (epiArray[i].season === 1) {
+      mainBody.innerHTML += generateMovies(epiArray[i], numberOfLikes[i].likes);
+    }
+  }
+  const likeBtn = document.querySelectorAll('.like-btn');
+  for (let i = 0; i < likeBtn.length; i++) {
+    likeBtn[i].addEventListener('click', async () => {
+      mainBody.innerHTML = '';
+      const epiArray = await apiGet();
+      await sendLike(epiArray[i].id);
+      location.reload();
+    });
+  }
+});
+
+const generateMovies = (episode, likes) => `
+    <div id="episode-card-${episode.number}" class="episode-card">
+      <div class="episode-img" id="episode-img-${episode.number}">
+        <img src="${episode.image.medium}">
       </div>
-      <div class = "episode-txt">
+      <div class="episode-txt">
         <p>Episode ${episode.number}</p>
-        <div class = "likes">
-          <div class = "like-btn-container"><img class = "like-btn" src = "/assets/unliked.png" alt = "unliked"></div>
-          <p class = "like-txt">5 likes</p>
+        <div class="likes">
+          <div class="like-btn-container">
+            <img class="like-btn" src="/assets/liked.png" alt="unliked">
+          </div>
+          <p class="like-txt">${likes}</p>
         </div>
       </div>
     </div>
   `;
-}
-
-window.addEventListener('load', async () => {
-  const epiArray = await apiGet();
-  epiArray.forEach((episode) => {
-    if (episode.season === 1) {
-      mainBody.innerHTML += generateMovies(episode);
-    }
-  });
-});
