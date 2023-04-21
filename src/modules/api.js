a538852d545eae94c9e9923c9e1a122ec7f40f4a
Mylo474 <antwieric@gmail.com>
2023-04-21 16:01:45 +0000

    solved git conflict

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,8 +1,39 @@
-const apiGet = async () => {
+export const apiGet = async () => {
   const req = await fetch('https://api.tvmaze.com/shows/4/episodes');
   const response = await req.json();
   return response;
 };
+
+// getting the id of the involvment API
+export const involvementApiId = async () => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
+    method: 'POST',
+  });
+  return [response, 'D378r3vCLkaRvnEDLjsU'];
+};
+
+// sending likes to the involvment API using the unique id
+export const sendLike = async (index) => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/D378r3vCLkaRvnEDLjsU/likes/', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: `${index}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response;
+};
+
+// getting likes from the involvement API
+export const getLikes = async () => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/D378r3vCLkaRvnEDLjsU/likes/', {
+    method: 'GET',
+  });
+  const numberOfLikes = await response.json();
+  return numberOfLikes;
+}
 
 const involvementApiGet = async (id) => {
   const req = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1TzK26frOiLTOPOwesN3/comments?item_id=item${id}`);
@@ -26,7 +57,6 @@ const involvementApiPost = async (id, username, comment) => {
 };
 
 export {
-  apiGet,
   involvementApiGet,
   involvementApiPost,
 };
